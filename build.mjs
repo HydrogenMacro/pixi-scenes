ea902e9375d3eef6af5525e4c536f240614c8e9e
HydrogenMacro <hydrogenmacro@gmail.com>
2024-05-23 14:53:36 +0000

    builds

---
+++ build.mjs
@@ -0,0 +1,38 @@
+import * as esbuild from 'esbuild'
+
+await esbuild.build({
+  entryPoints: ["src/pixi-scenes.mjs"],
+  bundle: true,
+  minify: false,
+  sourcemap: true,
+  format: "esm",
+  target: "es6",
+  outdir: "dist",
+  outExtension: { ".js": ".mjs" }
+});
+await esbuild.build({
+  entryPoints: ["src/pixi-scenes.mjs"],
+  bundle: true,
+  minify: false,
+  sourcemap: true,
+  target: "es6",
+  outdir: "dist",
+});
+await esbuild.build({
+  entryPoints: ["src/pixi-scenes.mjs"],
+  bundle: true,
+  minify: true,
+  sourcemap: true,
+  format: "esm",
+  target: "es6",
+  outdir: "dist",
+  outExtension: { ".js": ".mjs" }
+});
+await esbuild.build({
+  entryPoints: ["src/pixi-scenes.mjs"],
+  bundle: true,
+  minify: true,
+  sourcemap: true,
+  target: "es6",
+  outdir: "dist",
+});
